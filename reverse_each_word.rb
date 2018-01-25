64f0f8e0654195e2eb7ee455947a74ab9be1a5c4
Eden Mesfun <emesfun@gmail.com>
2018-01-25 00:20:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,6 @@ sentence1 = ["Hello there, and how are you?"]
 
 def reverse_each_word (sentence1)
   binding.pry
+    sentence1.split
     sentence1["Hello there, and how are you?"].collect{|string| string.reverse()}
 end #method
