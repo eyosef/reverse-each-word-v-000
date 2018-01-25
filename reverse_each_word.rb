0963ff59150ac01e26c8d6d52d22490971e377ec
Eden Mesfun <emesfun@gmail.com>
2018-01-25 00:28:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 # def reverse_each_word (sentence1)
 #     sentence1.reverse().split(" ").reverse().join(" ")
-# end #mjethod
+# end #method
 
 def reverse_each_word (sentence1)
     sentence1.split.collect{|string| string.reverse()}.join(" ")
